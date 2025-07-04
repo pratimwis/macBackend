eae967d45924bd3aa749c730c885e2da03e0f9b2
soumyawis <soumya98.wis@gmail.com>
2025-07-04 19:35:59 +0530

    bug fix

--- src/utils/jwtToken.ts
+++ src/utils/jwtToken.ts
@@ -7,7 +7,7 @@ export const genarateToken = (userId: string, res: Response) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+    secure: true, // Use secure cookies in production
     sameSite: "strict", // Helps prevent CSRF attacks
   });
   return token;
